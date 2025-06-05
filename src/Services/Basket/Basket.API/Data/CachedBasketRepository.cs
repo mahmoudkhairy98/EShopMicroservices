11afd39049c6f8230c314c94fe3c92a54e1a6fd7
mahmoudkhairy98 <mahmoudkhairy98@yahoo.com>
2025-06-05 12:50:06 +0400

    Section 9

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    //In this class basically we have implemented 2 patterns proxy and decortaor patterns
+    //For proxy pattern, CachedBasketRepository acts as a proxy and forwarding the calls to underlying basket repository
+    //we can see that we will be forwarding the get basket and store basket and delete basket operations using the underlying repository object that we have injected
+    //For decorator pattern, we will extend the functionality of basket repository by adding caching logic
+    public class CachedBasketRepository(IBasketRepository repository,IDistributedCache cache) : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string UserName, CancellationToken cancellationToken = default)
+        {
+            var cachedBasket = await cache.GetStringAsync(UserName, cancellationToken);
+            if(!string.IsNullOrEmpty(cachedBasket))
+                return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+
+            var basket = await repository.GetBasket(UserName, cancellationToken);
+            await cache.SetStringAsync(UserName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+        {
+            await repository.StoreBasket(basket, cancellationToken);
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+        public async Task<bool> DeleteBasket(string UserName, CancellationToken cancellationToken = default)
+        {
+            await repository.DeleteBasket(UserName, cancellationToken);
+            await cache.RemoveAsync(UserName, cancellationToken);
+            return true;
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,5 +1,8 @@
 
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Caching.Distributed;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,9 +29,32 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+//Here if we need to register 2 repository to IBasketRepository, it is not feasible to directly register multiple implementation to IBasketRepository and it makes
+//problem with direct DI and if we added this line builder.Services.AddScoped<IBasketRepository, CachedBasketRepository>(); , it will take the last registration which is 
+//CachedBasketRepository in this case and ignore the first registration BasketRepository so what is the solution?
+//The first solution that we can manullay decorate CachedBasketRepository as shown below
+//builder.Services.AddScoped<IBasketRepository>(provider =>
+//{
+//    var basketRepository = provider.GetRequiredService<BasketRepository>();
+//    return new CachedBasketRepository(basketRepository, provider.GetRequiredService<IDistributedCache>());
+//}); but as suugested this solution is not scalable and not managable and insted of that we should use scrutor library
+//Scrutor library is a library that simplifies the registration of decorators in DI container
+//Now, when the request sent, the handle method will call CachedBasketRepository first to check if there is data stored in the cache, if there is no data, it will call the DB
+//through BasketRepository and the purpose of this is to reduce calls to the DB to increase the performance otherwise we can use BasketRepository only without using CachedBasketRepository
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
 
 //we add this service in order to get formative exception if any exception happens sfter sending the request
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+//Add helath checks with its extension methods to check the health as this API is relying on postgres and redis
+builder.Services.AddHealthChecks()
+                .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
+                .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
 
 var app = builder.Build();
 
@@ -39,5 +65,14 @@ var app = builder.Build();
 app.MapCarter();
 
 app.UseExceptionHandler(opts => { });
+
+//this is the basic configuration of health check and the response will be "Healthy" if the API and iys backing services like DB is up & running
+//app.UseHealthChecks("/health");
+//If we need to make our health endpoint response as a json response including all entries, we have to install this package AspNetCore.HealthChecks.UI.Client and add the below
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
