a9ec155d2e9ad65deef9394394de447800f21383
mahmoudkhairy98 <mahmoudkhairy98@yahoo.com>
2025-06-09 12:49:15 +0400

    Section 13 Section 14 - Create Ordering.Domain Abstractions and Models in DDD

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class Order : Aggregate<Guid>
+    {
+        private readonly List<OrderItem> _orderItems = new();
+        //public IReadOnlyList<OrderItem> OrderItems { get { return _orderItems.AsReadOnly(); } }
+        public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();//same syntax like above but in different way by using Expression Bodied Property
+        public Guid CustomerId { get; private set; } = default!;
+        public Guid OrderId { get; private set; } = default!;
+        public Address ShippingAddress { get; private set; } = default!;
+        public Address BillingAddress { get; private set; } = default!;
+        public Payment Payment { get; private set; } = default!;
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+        public decimal TotalPrice
+        {
+            get
+            {
+                return OrderItems.Sum(x => x.Price * x.Quantity);
+            }
+            set { }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/Address.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    //we are using record here instead of class because it is very good in defining value objects in terms of comparing and removing ID operations
+    public record Address
+    {
+        public string FirstName { get; } = default!;
+        public string LsstName { get; } = default!;
+        public string? EmailAddress { get; } = default!;
+        public string AddressLine { get; } = default!;
+        public string Country { get; } = default!;
+        public string State { get; } = default!;
+        public string ZipCode { get; } = default!;
+    }
+}
