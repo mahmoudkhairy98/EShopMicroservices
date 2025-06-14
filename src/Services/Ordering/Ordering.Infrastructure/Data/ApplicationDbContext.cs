c2df245b0521b80b900cba7cc7aae6620d08d27f
mahmoudkhairy98 <mahmoudkhairy98@yahoo.com>
2025-06-14 22:01:17 +0400

    Section 15 - 208

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using System.Reflection;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //This ApplyConfigurationsFromAssembly method scans the assembly for all classes that implement IEntityTypeConfiguration and apply state configuration
+            //This ApplyConfigurationsFromAssembly method is called within OnModelCreating method
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}
