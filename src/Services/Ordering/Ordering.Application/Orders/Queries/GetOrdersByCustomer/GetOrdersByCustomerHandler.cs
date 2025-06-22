1c7ebae09dd8c65fe382ab4f5352ca690c8c3f35
mahmoudkhairy98 <mahmoudkhairy98@yahoo.com>
2025-06-22 15:56:00 +0400

    Section 16 - 246

---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer
+{
+    public class GetOrdersByCustomerHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+    {
+        public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+        {
+            //get orders by customer using dbContext
+            //return result
+
+            var orders = await dbContext.Orders
+                    .Include(o => o.OrderItems)
+                    .AsNoTracking()//this is a good optimization when you are only reading data and don't plan to update these entiries in the same context
+                    .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+                    .OrderBy(o => o.OrderName.Value)
+                    .ToListAsync(cancellationToken);
+
+            return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+        }
+    }
+}
