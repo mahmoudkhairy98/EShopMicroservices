aaaa1c66649184a776cdf3a5ba9fcaf87eb73848
mahmoudkhairy98 <mahmoudkhairy98@yahoo.com>
2025-06-24 16:39:36 +0400

    Section 17 - 263

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -1,13 +1,17 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services,IConfiguration configuration)
         {
             services.AddCarter();
             services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                    .AddSqlServer(configuration.GetConnectionString("Database")!);
 
             return services;
         }
@@ -15,6 +19,10 @@ namespace Ordering.API
         {
             app.MapCarter();
             app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health", new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
 
             return app;
         }
